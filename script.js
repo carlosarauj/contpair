c54fe05019ab2e9de0b65f0204899867007792e4
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2023-10-01 19:34:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+start = 10
+end = 20
+for(let start = 10; start <= end; start++){
+    console.log(start++)
+    
+}
+
+//outra maneira
+/* for (let cont = 10; cont <= 20; cont++) {
+    if (cont % 2 === 0) {
+      console.log(cont); // Imprime apenas números pares
+    }
+  } */
